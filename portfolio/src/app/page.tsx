4d13a50e3f2a4f5a29613942be439d0764ff4f23
Hannah-C <choihannah2003@gmail.com>
2024-01-10 03:00:02 -0500

    first push

---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,15 @@
+import HeaderSection from "@/components/HeaderSection";
+import NavBar from "@/components/navBar";
+import Image from "next/image";
+import { Container } from "postcss";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-hannahs-colour">
+      <NavBar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeaderSection />
+      </div>
+    </main>
+  );
+}
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'hannahs-colour': '#121212',
+        'hannahs-colour-2': '#181818',
+        'hannahs-text': '#ADB7BE',
+      },
+    },
+  },
+  plugins: [],
+}
